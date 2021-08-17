1a5c74dd258859aeb6ef6f2ce2c05932738b7a02
Vinicius <v.honorato@live.com>
2021-08-17 14:14:18 -0800

    feat(): Mass data

--- aulas/mod_11/cypress/integration/login.spec.js
+++ aulas/mod_11/cypress/integration/login.spec.js
@@ -1,8 +1,9 @@
 /// <reference types="cypress"/>
+const perfil = require("../fixtures/perfil.json");
 
 context("Funcionalidade Login", () => {
   beforeEach(() => {
-    cy.visit("http://lojaebac.ebaconline.art.br/minha-conta/");
+    cy.visit("minha-conta");
   });
 
   afterEach(() => {
@@ -19,6 +20,28 @@ context("Funcionalidade Login", () => {
       "contain",
       "Olá, aluno_ebac (não é aluno_ebac? Sair)"
     );
+  });
+
+  it("Deve fazer login com sucesso - Usando arquivo de dados", () => {
+    cy.get("#username").type(perfil.usuario);
+    cy.get("#password").type(perfil.senha);
+    cy.get(".woocommerce-form > .button").click();
+
+    cy.get(".page-title").should("contain", "Minha conta");
+    cy.get(".woocommerce-MyAccount-content > :nth-child(2)").should(
+      "contain",
+      "Olá, aluno_ebac (não é aluno_ebac? Sair)"
+    );
+  });
+
+  it.only("Deve fazer login com sucesso - Usando fixture", () => {
+    cy.fixture("perfil").then((dados) => {
+      cy.get("#username").type(dados.usuario);
+      cy.get("#password").type(dados.senha, {log: false});
+      cy.get(".woocommerce-form > .button").click();
+
+      cy.get(".page-title").should("contain", "Minha conta");
+    });
   });
 
   it("Deve exibir uma mensagem de erro ao inserir usuário inválido", () => {
---
+++ exercises/mod_11/cypress/integration/mynutri.spec.js
@@ -0,0 +1,10 @@
+/// <reference types="cypress"/>
+
+describe("Mynutri test", () => {
+  beforeEach(() => {
+    cy.visit("https://mynutri-staging.firebaseapp.com/");
+  });
+  it('teste login', () => {
+      
+  });
+});
