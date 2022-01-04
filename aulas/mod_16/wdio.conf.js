ada34276130521f7854074b73e2897a4609a419f
Vinicius <v.honorato@live.com>
2022-01-03 19:05:31 -0800

    chore: browserstack and appium class configuration

--- aulas/mod_16/wdio.conf.js
+++ aulas/mod_16/wdio.conf.js
@@ -3,30 +3,30 @@ const allure = require("allure-commandline");
 const video = require("wdio-video-reporter");
 
 exports.config = {
-  //hostname: "localhost",
-  //port: 4723,
-  user: "viniciushonorato_AUMh1o",
-  key: "FsnkJurUaVqxjExYGuEf",
-  //services: ["appium"],
-  services: ["browserstack"],
-  //path: "/wd/hub",
+  hostname: "localhost",
+  port: 4723,
+  // user: "viniciushonorato_AUMh1o",
+  // key: "FsnkJurUaVqxjExYGuEf",
+  services: ["appium"],
+  // services: ["browserstack"],
+  path: "/wd/hub",
   specs: ["./test/specs/**/*.spec.js"],
   framework: "mocha",
   capabilities: [
     {
-    //   platformName: "Android",
-    //   platformVersion: "11",
-    //   deviceName: "ebac-qe",
-    //   automationName: "UiAutomator2",
-    //   app: join(process.cwd(), "./app/android/loja-ebac.apk"),
-    //   appWaitActivity: "com.woocommerce.android.ui.login.LoginActivity",
-    //   newCommandTimeout: 240
-    'app' : 'bs://35cdf16b32be3498addb8929351d5508ad34b2c9',
-    'device' : 'Samsung Galaxy S8',
-    'os_version' : '7.0',
-    'project' : 'Meu primeiro projeto em device farm',
-    'build' : 'Node Android',
-    'name': 'test_login'
+      platformName: "Android",
+      platformVersion: "11",
+      deviceName: "ebac-qe",
+      automationName: "UiAutomator2",
+      app: join(process.cwd(), "./app/android/loja-ebac.apk"),
+      appWaitActivity: "com.woocommerce.android.ui.login.LoginActivity",
+      newCommandTimeout: 240
+    // 'app' : 'bs://35cdf16b32be3498addb8929351d5508ad34b2c9',
+    // 'device' : 'Samsung Galaxy S8',
+    // 'os_version' : '7.0',
+    // 'project' : 'Meu primeiro projeto em device farm',
+    // 'build' : 'Node Android',
+    // 'name': 'test_login'
     },
   ],
   waitforTimeout: 20000,
--- exercises/mod_16/wdio.conf.js
+++ exercises/mod_16/wdio.conf.js
@@ -1,24 +1,33 @@
 const { join } = require("path");
 
 exports.config = {
-  hostname: "localhost",
-  port: 4723,
-  path: "/wd/hub",
+  //hostname: "localhost",
+  //port: 4723,
+  //path: "/wd/hub",
+  user: "viniciushonorato_AUMh1o",
+  key: "FsnkJurUaVqxjExYGuEf",
   specs: ["./test/specs/**/*.spec.js"],
   framework: "mocha",
-  services: ["appium"],
+  //services: ["appium"],
+  services: ["browserstack"],
   capabilities: [
     {
-      platformName: "Android",
-      maxInstances: 1,
-      platformVersion: "11",
-      deviceName: "webdriveIO",
-      automationName: "UiAutomator2",
-      app: join(process.cwd(), "./app/android/Android-NativeDemoApp-0.4.0.apk"),
-      appPackage: "com.wdiodemoapp",
-      appActivity: "com.wdiodemoapp.MainActivity",
-      appWaitActivity: "com.wdiodemoapp.MainActivity",
-      newCommandTimeout: 240
+      // platformName: "Android",
+      // maxInstances: 1,
+      // platformVersion: "11",
+      // deviceName: "webdriveIO",
+      // automationName: "UiAutomator2",
+      // app: join(process.cwd(), "./app/android/Android-NativeDemoApp-0.4.0.apk"),
+      // appPackage: "com.wdiodemoapp",
+      // appActivity: "com.wdiodemoapp.MainActivity",
+      // appWaitActivity: "com.wdiodemoapp.MainActivity",
+      // newCommandTimeout: 240
+      'app' : 'bs://75fb8213242d0c48cf1597688f405c969380932a',
+      'device' : 'Samsung Galaxy A51',
+      'os_version' : '10.0',
+      'project' : 'Exercício Módulo 17',
+      'build' : '1',
+      'name': 'form and login tests'
     },
   ],
   waitforTimeout: 20000,
